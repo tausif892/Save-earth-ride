2e8e28a7d422443cd853420e20fa14080c4778ed
Satvik <132929264+ItsSatviK13@users.noreply.github.com>
2025-07-18 23:14:12 +0000

    modified:   lib/googlesheet.ts

--- lib/googlesheet.ts
+++ lib/googlesheet.ts
@@ -1,7 +1,11 @@
 import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
+import path from 'path';
 
-const SPREADSHEET_ID = '1JQOP0BInND1IVZZv29IkYmjJfl_Zl5LW68R57DS2iAg';
+const keyPath = path.resolve(process.cwd(), 'lib/credentials.json');
+
+const SPREADSHEET_ID = /*'1ai04GFQ-aCVzi54x2ZeuDXtasd7Y8ATCr7R1bSqMrNQ';*/
+'1JQOP0BInND1IVZZv29IkYmjJfl_Zl5LW68R57DS2iAg';
 
 const categories = [
   'drives',
@@ -17,21 +21,13 @@ const categories = [
 ];
 
 const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: 'service_account',
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    // auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    // token_uri: 'https://oauth2.googleapis.com/token',
-  },
+  keyFile: keyPath,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 async function getSheetsClient() {
   const authClient = await auth.getClient() as JWT;
+
   const sheets = google.sheets({ version: 'v4', auth: authClient });
   return sheets;
 }
